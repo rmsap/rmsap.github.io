5402c724f81543c419229c0e0382bcc0bd27ca1d
rmsap <87085375+rmsap@users.noreply.github.com>
2022-08-03 15:34:57 -0400

    Added additional pages

---
+++ components/header.js
@@ -0,0 +1,49 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+    <style>
+        nav {
+            height: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color:  #0a0a23;
+        }
+
+        header {
+            display: inline;
+        }
+
+        a {
+            font-weight: 700;
+            margin: 0 25px;
+            color: #fff;
+            text-decoration: none;
+        }
+
+        a:hover {
+            padding-bottom: 5px;
+            box-shadow: inset 0 -2px 0 0 #fff;
+        }
+    </style>
+    <header>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="projects.html">Projects</a>
+            <a href="about.html">About</a>
+        </nav>
+    </header>
+`;
+
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+        shadowRoot.appendChild(headerTemplate.content);
+    }
+}
+
+customElements.define('header-component', Header);
