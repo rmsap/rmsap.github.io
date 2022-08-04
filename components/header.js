5512190c53894fe6d60c8b8bc4a3d984b5ada05f
rmsap <87085375+rmsap@users.noreply.github.com>
2022-08-04 11:05:47 -0400

    Added dropdown menu to projects.

--- components/header.js
+++ components/header.js
@@ -25,11 +25,50 @@ headerTemplate.innerHTML = `
             padding-bottom: 5px;
             box-shadow: inset 0 -2px 0 0 #fff;
         }
+
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
     </style>
     <header>
         <nav>
             <a href="index.html">Home</a>
-            <a href="projects.html">Projects</a>
+            <div class="dropdown">
+              <a href="projects.html">Projects</a>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
             <a href="about.html">About</a>
         </nav>
     </header>
